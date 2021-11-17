ad653319cc02ace6cb6f28bcdb64d6f50e0ebb23
Krzysztof Ostrowski <ostrowski021@gmail.com>
2021-11-17 23:26:21 +0100

    Initial Commit for NumbersInCSharp

---
+++ NumbersInCSharp/NumbersInCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+
+// subtraction
+c = a - b;
+Console.WriteLine(c);
+
+// multiplication
+c = a * b;
+Console.WriteLine(c);
+
+// division
+c = a / b;
+Console.WriteLine(c);
+Console.ReadLine();
