860bfd3fc1ff02dfb292ed627839acc1ea463f14
Krzysztof Ostrowski <ostrowski021@gmail.com>
2021-11-17 23:54:39 +0100

    NumbersInCSharp integer exercise finished

--- NumbersInCSharp/NumbersInCSharp/Program.cs
+++ NumbersInCSharp/NumbersInCSharp/Program.cs
@@ -43,4 +43,19 @@ void OrderPrecedence()
     int g = 3;
     int h = (e + f) / g;
     Console.WriteLine(h);
+
+    int i = 7;
+    int j = 4;
+    int k = 3;
+    int l = (i + j) / k;
+    int m = (i + j) % k;
+    Console.WriteLine($"quotient: {l}");
+    Console.WriteLine($"remainder: {m}");
+
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"The range of integers is {min} to {max}");
+
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow: {what}");
 }
