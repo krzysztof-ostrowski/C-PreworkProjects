bd909d6220c079c777a7cfff61264287140ad9b5
Krzysztof Ostrowski <ostrowski021@gmail.com>
2021-11-18 00:03:27 +0100

    WorkWithDoubles added to NumbersInCSharp

--- NumbersInCSharp/NumbersInCSharp/Program.cs
+++ NumbersInCSharp/NumbersInCSharp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 //WorkWithIntegers();
-OrderPrecedence();
+//OrderPrecedence();
 void WorkWithIntegers()
 { 
 
@@ -58,4 +58,24 @@ void OrderPrecedence()
 
     int what = max + 3;
     Console.WriteLine($"An example of overflow: {what}");
+
+
 }
+double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+double e = 19;
+double f = 23;
+double g = 8;
+double h = (e + f) / g;
+Console.WriteLine(h);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
